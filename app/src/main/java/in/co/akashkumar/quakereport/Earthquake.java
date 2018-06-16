ac8b9058388c5b9458858f16e563d66a6208ef0d
akash-kmr <bholuakku@gmail.com>
2018-06-16 13:26:16 +0530

    Initial commit -Added Earthquake class and Earthquake adapter class

---
+++ app/src/main/java/in/co/akashkumar/quakereport/Earthquake.java
@@ -0,0 +1,25 @@
+package in.co.akashkumar.quakereport;
+
+public class Earthquake {
+    private String magnitude;
+    private String location;
+    private String date;
+
+    public Earthquake(String magnitude, String location, String date) {
+        this.magnitude = magnitude;
+        this.location = location;
+        this.date = date;
+    }
+
+    public String getMagnitude() {
+        return magnitude;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getDate() {
+        return date;
+    }
+}
